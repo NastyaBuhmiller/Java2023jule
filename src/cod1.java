11b0a6d20e62eec88621d835d0f61a39803ee73b
NastyaBuhmiller <nbuhmiller1997@mail.ru>
2023-07-21 17:37:48 +0600

    One commit Добавила файл для примера

---
+++ src/cod1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class cod1 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String line;
+        line = s.nextLine();
+        System.out.println(line);
+        }
+}
