5754e73a2ade24f00beaf8c801d050d00d10b66f
NastyaBuhmiller <nbuhmiller1997@mail.ru>
2023-07-21 17:45:10 +0600

    Еще добавила кое-что

--- src/cod1.java
+++ src/cod1.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class cod1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String line;
         line = s.nextLine();
-        System.out.println(line);
+        if (line.isEmpty()) {
+            System.out.println("Исходная строка пуста");
+        } else {
+            System.out.println(line);
         }
+    }
 }
