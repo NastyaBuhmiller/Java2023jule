e7b0191603c18c95ef113aa2331251ed80d7299e
NastyaBuhmiller <nbuhmiller1997@mail.ru>
2023-07-28 20:00:26 +0600

    добавлена конфигурация для имени и емейла начало решения задачи пр9№2 с помощью коллекции

---
+++ src/com/company/Task7834.java
@@ -0,0 +1,143 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task7834 {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Первый тест");
+        String line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println("Второй тест");
+        line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 2 & arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println("Третий тест");
+        line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 2 || arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 2 & arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println("Четвертый тест");
+        line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 3 || arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 3 || arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println("Пятый тест");
+        line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 3 || arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 3 || arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println("Шестой тест");
+        line = s.nextLine();
+        arr.add(line);
+        System.out.println("В коллекции длиной " + arr.size());
+        System.out.println("Значение элемента на индексе " + 0 + " это " + arr.get(0));
+        if (arr.size() > 3 & arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 2 + " это " + arr.get(2));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 2);
+        }
+        if (arr.size() > 3 & arr.size() > 5) {
+
+            System.out.println("Значение элемента на индексе " + 3 + " это " + arr.get(3));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 3);
+        }
+        if (arr.size() > 3 & arr.size() > 5) {
+            System.out.println("Значение элемента на индексе " + 5 + " это " + arr.get(5));
+        } else {
+            System.out.println(" Нет элемента на индексе " + 5);
+        }
+        System.out.println();
+    }
+}
+
+
+
+
