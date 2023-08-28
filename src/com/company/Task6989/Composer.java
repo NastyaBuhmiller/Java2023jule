01220e230638a2351ea71c107995ad94a17699c6
NastyaBuhmiller <nbuhmiller1997@mail.ru>
2023-08-28 19:31:49 +0600

    pr14, task 5 with classes

---
+++ src/com/company/Task6989/Composer.java
@@ -0,0 +1,8 @@
+package com.company.Task6989;
+
+public class Composer {
+    public String composer_id;
+    public String first_name;
+    public String last_name;
+    public String birth_place;
+}
