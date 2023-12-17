09ce2f8b2eeb2644f6f88bf546e5d0fa9a4cd969
NastyaBuhmiller <nbuhmiller1997@mail.ru>
2023-12-17 21:14:12 +0600

    17/12/2023

--- src/com/company/http/Films.java
+++ src/com/company/http/Films.java
@@ -13,10 +13,14 @@ public class Films {
         File category = new File("RES/category.dat");
         File film_category = new File("RES/film_category.dat");
         File name_film = new File("RES/film.dat");
+        File id_actor = new File("RES/film_actor.dat");
+        File name_actor = new File("RES/actor.dat");
         try {
             Scanner scanner_category = new Scanner(category);
             Scanner scanner_film_category = new Scanner(film_category);
             Scanner scanner_name_film = new Scanner(name_film);
+            Scanner scanner_id_actor = new Scanner(id_actor);
+            Scanner scanner_name_actor = new Scanner(name_actor);
             Scanner k = new Scanner(System.in);
             String value = k.nextLine();
             String[] num;
@@ -52,8 +56,34 @@ public class Films {
                     System.out.println(arr1[1]);
                 }
             }
+            while (scanner_category.hasNext()) {
+                String line1 = scanner_category.nextLine();
+                if (line1.isEmpty()) {
+                } else {
+                    num = line1.split("\t");
+                    if (num.length < 2) {
+                    } else {
+                        System.out.println(num[1]);
+                    }
+                }
+            }
+            while (scanner_id_actor.hasNext()) {
+                String[] arr;
+                String line2 = scanner_id_actor.nextLine();
+                arr = line2.split("\t");
+                if (arr.length > 2 && arr[1].equals(i)) {
+                    i_category.add(arr[0]);
 
-
+                }
+            }
+            while (scanner_name_actor.hasNext()) {
+                String[] arr1;
+                String line3 = scanner_name_actor.nextLine();
+                arr1 = line3.split("\t");
+                if (arr1.length > 2 && i_category.contains(arr1[0])) {
+                    System.out.println(arr1[1]);
+                }
+            }
         } catch (IOException e) {
             System.out.println(film_category.getAbsolutePath());
             e.printStackTrace();
--- src/com/company/http/Server_Films.java
+++ src/com/company/http/Server_Films.java
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -54,37 +53,37 @@ public class Server_Films {
         }
     }
 
-    static class Name_filmHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) { //переменная t хранит запрос и ответ на него
-            File file_film = new File("RES/film.dat");
-            File file_category = new File("RES/category.dat");
-            File file_film_category = new File("RES/film_category.dat");
-            try {
-                Scanner scanner_film = new Scanner(file_film);
-                Scanner scanner_category = new Scanner(file_category);
-                Scanner scanner_film_category = new Scanner(file_film_category);
-                if (!file_film.exists()) {
-                    file_film.createNewFile();
-                }
-                String head = Head();
-                String foot = Foot();
-                String bodyCircle_open = BodyCircle_open();
-                String body_close = Body_close();
-                String file;
-                String line = ReadFilm(scanner_film, scanner_category, scanner_film_category, t);
-                file = head + bodyCircle_open + "<ul>" +
-                        line + "</ul>" + body_close + foot;
-                t.sendResponseHeaders(200, file.getBytes().length);// http статус код (200-ок)
-                PrintWriter writer1 = new PrintWriter(t.getResponseBody());// подготовка инструмента для записи данных в ответ
-                writer1.write(file);
-                writer1.close();
-            } catch (IOException e) {
-                System.out.println(file_film.getAbsolutePath());
-                e.printStackTrace();
+    public static String Read_actor(Scanner scanner_film_category, Scanner scanner_id_actor, Scanner scanner_name_actor, HttpExchange t) {
+        String total = "";
+        ArrayList<String> i_category = new ArrayList<>();
+        String line1 = t.getRequestURI().getQuery();
+        String id = FromAdressLineName_actorToWriter(line1);
+        while (scanner_film_category.hasNext()) {
+            String[] arr;
+            String line2 = scanner_film_category.nextLine();
+            arr = line2.split("\t");
+            if (arr.length > 2 && arr[1].equals(id)) {
+                i_category.add(arr[0]);
             }
-
         }
+        while (scanner_id_actor.hasNext()) {
+            String[] arr;
+            String line2 = scanner_id_actor.nextLine();
+            arr = line2.split("\t");
+            if (arr.length > 2 && arr[1].equals(id)) {
+                i_category.add(arr[0]);
+            }
+        }
+        while (scanner_name_actor.hasNext()) {
+            String[] arr1;
+            String line3 = scanner_name_actor.nextLine();
+            arr1 = line3.split("\t");
+            if (arr1.length > 2 && i_category.contains(arr1[0])) {
+                total = total + "<li><a href=\"film?id=" + arr1[0] + "\">" + arr1[1] + "</a>" + "</li>";
+            }
+        }
+
+        return total;
     }
 
     static class Info_filmHandler implements HttpHandler {
@@ -119,6 +118,7 @@ public class Server_Films {
 
         }
     }
+
 
     public static String Head() {
         return ("<!DOCTYPE html>\n" +
@@ -159,6 +159,14 @@ public class Server_Films {
             }
         }
         return total;
+    }
+
+    public static String FromAdressLineName_actorToWriter(String line) {
+        String[] arr = line.split("&");
+        String id;
+        int index = arr[0].indexOf("=");
+        id = arr[0].substring(index + 1);
+        return id;
     }
 
     public static String ReadFilm(Scanner scanner_film, Scanner scanner_category, Scanner scanner_film_category, HttpExchange t) {
@@ -204,6 +212,43 @@ public class Server_Films {
         return id;
     }
 
+    static class Name_filmHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) { //переменная t хранит запрос и ответ на него
+            File file_film = new File("RES/film.dat");
+            File file_category = new File("RES/category.dat");
+            File file_film_category = new File("RES/film_category.dat");
+            File id_actor = new File("RES/film_actor.dat");
+            File name_actor = new File("RES/actor.dat");
+            try {
+                Scanner scanner_film = new Scanner(file_film);
+                Scanner scanner_category = new Scanner(file_category);
+                Scanner scanner_film_category = new Scanner(file_film_category);
+                Scanner scanner_id_actor = new Scanner(id_actor);
+                Scanner scanner_name_actor = new Scanner(name_actor);
+                if (!file_film.exists()) {
+                    file_film.createNewFile();
+                }
+                String head = Head();
+                String foot = Foot();
+                String bodyCircle_open = BodyCircle_open();
+                String body_close = Body_close();
+                String file;
+                String line = ReadFilm(scanner_film, scanner_category, scanner_film_category, t);
+                String name_actor = Read_actor(scanner_film_category, scanner_id_actor, scanner_name_actor, t);
+                file = head + bodyCircle_open + "<ul>" +
+                        line + name_actor + "</ul>" + body_close + foot;
+                t.sendResponseHeaders(200, file.getBytes().length);// http статус код (200-ок)
+                PrintWriter writer1 = new PrintWriter(t.getResponseBody());// подготовка инструмента для записи данных в ответ
+                writer1.write(file);
+                writer1.close();
+            } catch (IOException e) {
+                System.out.println(file_film.getAbsolutePath());
+                e.printStackTrace();
+            }
+
+        }
+    }
     public static String FromAdressLineid_filmToWriter(String line) {
         String[] arr = line.split("&");
         String id;
